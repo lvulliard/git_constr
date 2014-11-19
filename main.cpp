2795fa9701d80d4a9f376aec985325c804a96659
Vulliard Loan <loan.vulliard@insa-lyon.fr>
2014-11-19 15:25:47 +0100

    Test on auto-generated constructors

--- main.cpp
+++ main.cpp
@@ -4,5 +4,10 @@
 int main(int argc, char** argv)
 {
 	printf("Hello	\n");
-	Simple_obj a;
+	Simple_obj obj1;
+	Simple_obj obj2();//obj2() calls the default constructor, but does not declare an object
+	Simple_obj obj3 = Simple_obj();
+	printf("nbvals:\nobj1:%d\nobj3:%d\n",obj1.get_nb(),obj3.get_nb());
+	Simple_obj* objDyn = new Simple_obj;
+	printf("objDyn:%d\n",(*objDyn).get_nb());
 }
