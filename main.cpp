26bd9cf5d2d134eeb111ddb4cbe44f95e248468d
Vulliard Loan <loan.vulliard@insa-lyon.fr>
2014-11-19 14:21:53 +0100

    Simple main function and basic class with only the 2 arguments

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include "simple_obj.h"
+
+int main(int argc, char** argv)
+{
+	printf("Hello	\n");
+	Simple_obj a;
+}
---
+++ simple_obj.h
@@ -0,0 +1,16 @@
+#ifndef SIM_INCLUDED
+#define SIM_INCLUDED
+#include <cstdio>
+
+class Simple_obj
+{
+public:
+	/*simple_obj(arguments);
+	~simple_obj();*/
+
+	int nb_vals;
+	int* vals;
+};
+
+
+#endif
